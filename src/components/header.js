7da5a84c0feca8cf81ebc91d6748e22a17f68844
lancaster-coder1991 <grjscott1991@hotmail.com>
2021-01-12 15:16:16 +0000

    experiment with adding styles to components via file imports

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import "./header.css"
 
 const Header = ({ siteTitle }) => (
   <header
@@ -16,7 +17,7 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h1 className="" style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,19 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+const wrapperStyles = {
+  maxWidth: `300px`,
+  marginBottom: `1.45rem`,
+  lineHeight: 1.5,
+}
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people yo</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <div style={wrapperStyles}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
