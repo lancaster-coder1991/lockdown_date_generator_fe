23545bb33674e2014f570bdc061089f71049bb08
lancaster-coder1991 <grjscott1991@hotmail.com>
2021-01-15 11:09:37 +0000

    refactor header component with styled components over modules

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,16 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styled from "styled-components"
 import styles from "./header.module.css"
 
+const SiteHeader = styled.header`
+  background: rebeccapurple;
+  margin-bottom: 1.45rem;
+`
+
 const Header = ({ siteTitle }) => (
-  <header className={styles.header}>
+  <SiteHeader className={styles.header}>
     <div className={styles.inner}>
       <h1 className={styles.heading}>
         <Link className={styles.link} to="/">
@@ -12,7 +18,7 @@ const Header = ({ siteTitle }) => (
         </Link>
       </h1>
     </div>
-  </header>
+  </SiteHeader>
 )
 
 Header.propTypes = {
